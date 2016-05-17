9fd0eb1125b50d44433dfd3b74b3c11b460faf0c
maqboolurrahim <maqboolurrahim@gmail.com>
2016-05-17 11:05:37 +0500

    all done fadeToggle added much more optimize and minified file added

--- dataplx.js
+++ dataplx.js
@@ -1,29 +1,33 @@
 dataplx = {
 	/* CORE HANDLERS */
 	config: {},
+	animations: ["fade", "shrink"],
 	init: function() {
 		$('[data-plx-close]').click(dataplx.close);
 		$('[data-plx-toggle]').click(dataplx.toggle);
 	},
 	operate: function() {
-		config = dataplx.config;
-		do_fade = config.ele.hasAttribute('data-plx-fade');
-		do_shrink = config.ele.hasAttribute('data-plx-shrink');
-		if (do_fade) {
-			dataplx.fade();
-		} else if (do_shrink) {
-			dataplx.shrink();
-		} else {
+		var found = 0;
+		var config = dataplx.config;
+		$.each(dataplx.animations, function(index, value) {
+			var ani = config.ele.hasAttribute("data-plx-" + value);
+			if (ani) {
+				dataplx[value]();
+				found = 1;
+				return false;
+			}
+		});
+		if (!found) {
 			dataplx.default();
 		}
 	},
-	remover: function () {
-		config.doOn.remove();
+	remover: function() {
+		dataplx.config.doOn.remove();
 	},
 	/* End of core handlers
 	 * CORE OPERATIONS
 	 */
-	toggle: function  () {
+	toggle: function() {
 		dataplx.config.ele = this;
 		var cls_attr = $(this).data('plx-toggle');
 		dataplx.config.doOn = cls_attr ? $(cls_attr).first() : $(this).next();
@@ -42,32 +46,31 @@ dataplx = {
 	/* CORE ANIMATIONS
 	 */
 	fade: function() {
-		if (config.doWhat == 'toggle' ) {
-			if ( config.doOn.is(':visible') )
-				config.doOn.fadeOut($(config.ele).data('plx-fade') || 1000);
-			else
-				config.doOn.fadeIn($(config.ele).data('plx-fade') || 1000);
+		var config = dataplx.config;
+		if (config.doWhat == 'toggle') {
+			config.doOn.fadeToggle($(config.ele).data('plx-fade') || 1000);
 		} else {
 			config.doOn.fadeOut($(config.ele).data('plx-fade') || 1000, dataplx.remover);
 		}
 	},
 	shrink: function() {
-		if (config.doWhat == 'toggle' ) {
-			if ( config.doOn.is(':visible') )
-				config.doOn.slideUp( $(config.ele).data('plx-shrink') || 1000);
+		var config = dataplx.config;
+		if (config.doWhat == 'toggle') {
+			if (config.doOn.is(':visible'))
+				config.doOn.slideUp($(config.ele).data('plx-shrink') || 1000);
 			else {
-				config.doOn.slideDown( $(config.ele).data('plx-shrink') || 1000);
+				config.doOn.slideDown($(config.ele).data('plx-shrink') || 1000);
 			}
 		} else {
-			config.doOn.slideUp( $(config.ele).data('plx-shrink') || 1000, dataplx.remover);
+			config.doOn.slideUp($(config.ele).data('plx-shrink') || 1000, dataplx.remover);
 		}
 	},
 	default: function() {
-		if (config.doWhat == 'toggle' )
+		var config = dataplx.config;
+		if (config.doWhat == 'toggle')
 			config.doOn.toggle();
 		else
 			dataplx.remover();
-	}
-
+	},
 };
 $(document).ready(dataplx.init);
